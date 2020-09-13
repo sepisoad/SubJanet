229a4eb05c41a2748ade3998ade240622843c249
sepisoad <sepehr.aryani@gmail.com>
2020-09-13 23:25:09 +0430

    renamed plugin old name and removed unneeded code

--- plg.py
+++ plg.py
@@ -6,89 +6,36 @@ import signal
 import sublime
 import sublime_plugin
 
-from .plg_nrepl_server import start as start_server
-from .plg_nrepl_server import stop as stop_server
-from .plg_nrepl_client import start as start_client
-from .plg_nrepl_client import stop as stop_client
-from .plg_nrepl_client import commit as nrepl_commit
-from .plg_nrepl_client import ping as nrepl_ping
 from .plg_format_code import format_code
 from .plg_generate_completion import generate_completion
 
-# ============================================================================ #
-#     _       _____ _      ____  ____           _____
-#  /\| |/\   / ____| |    / __ \|  _ \   /\    / ____|
-#  \ ` ' /  | |  __| |   | |  | | |_) | /  \  | (___
-# |_     _| | | |_ | |   | |  | |  _ < / /\ \  \___ \
-#  / , . \  | |__| | |___| |__| | |_) / ____ \ ____) |
-#  \/|_|\/   \_____|______\____/|____/_/    \_\_____/
-# ============================================================================ #
+#   _____ _      ____  ____          _       _____
+#  / ____| |    / __ \|  _ \   /\   | |     / ____|
+# | |  __| |   | |  | | |_) | /  \  | |    | (___
+# | | |_ | |   | |  | |  _ < / /\ \ | |     \___ \
+# | |__| | |___| |__| | |_) / ____ \| |____ ____) |
+#  \_____|______\____/|____/_/    \_\______|_____/
 
-DEFAULT_SETTINGS = 'Jide.sublime-settings'
-PLUGIN_TEMP_DIR = 'Jide'
+DEFAULT_SETTINGS = 'subjanet.sublime-settings'
+PLUGIN_TEMP_DIR = 'subjanet'
 PLUGIN_REPL_PROC = 'repl-proc'
 JANET_EXEC = 'janet_executable'
 JPM_EXEC = 'jpm_executable'
 JPM_OPT_LIST = 'list-installed'
 JPM_LIB_SPORK = 'spork'
-NREPL_HOST = 'nrepl_host'
-NREPL_PORT = 'nrepl_port'
-
-server_ref = None
-client_ref = None
-symbols_map = {}
 g_completion_list = []
 
-# ============================================================================ #
-#           _____ ____  __  __ __  __          _   _ _____   _____
-#    _     / ____/ __ \|  \/  |  \/  |   /\   | \ | |  __ \ / ____|
-#  _| |_  | |   | |  | | \  / | \  / |  /  \  |  \| | |  | | (___
-# |_   _| | |   | |  | | |\/| | |\/| | / /\ \ | . ` | |  | |\___ \
-#   |_|   | |___| |__| | |  | | |  | |/ ____ \| |\  | |__| |____) |
-#          \_____\____/|_|  |_|_|  |_/_/    \_\_| \_|_____/|_____/
-# ============================================================================ #
+#   _____ ____  __  __ __  __          _   _ _____   _____
+#  / ____/ __ \|  \/  |  \/  |   /\   | \ | |  __ \ / ____|
+# | |   | |  | | \  / | \  / |  /  \  |  \| | |  | | (___
+# | |   | |  | | |\/| | |\/| | / /\ \ | . ` | |  | |\___ \
+# | |___| |__| | |  | | |  | |/ ____ \| |\  | |__| |____) |
+#  \_____\____/|_|  |_|_|  |_/_/    \_\_| \_|_____/|_____/
 
 ##
-## JideStartNreplServerCommand
+## SubjanetFormatCommand
 ##
-class JideStartNreplServerCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
-    if not start_nrepl_process():
-      sublime.error_message('failed to start janet nrepl server process')      
-      return
-    set_nrepl_status_flag(True)
-
-##
-## JideStopNreplServerCommand
-##
-class JideStopNreplServerCommand(sublime_plugin.TextCommand):
-  def run(self, edit):    
-    terminate_nrepl_proc()
-    set_nrepl_status_flag(False)
-
-##
-## JideStartNreplClientCommand
-##
-class JideStartNreplClientCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
-    global client_ref    
-    janet = configs_get(JANET_EXEC)
-    host = configs_get(NREPL_HOST)
-    port = int(configs_get(NREPL_PORT))
-    client_ref = start_client(host, port)
-
-##
-## JideStopNreplClientCommand
-##
-class JideStopNreplClientCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
-    global client_ref    
-    stop_client(client_ref)
-
-##
-## JideFormatCommand
-##
-class JideFormatCommand(sublime_plugin.TextCommand):
+class SubjanetFormatCommand(sublime_plugin.TextCommand):
   def run(self, edit):
     if not is_janet_file(self.view):
       print('this is not a janet file')
@@ -100,14 +47,12 @@ class JideFormatCommand(sublime_plugin.TextCommand):
       return
     self.view.replace(edit, sublime.Region(0, file_size), formatted)    
 
-# ============================================================================ #
-#          ________      ________ _   _ _______ _____
-#    _    |  ____\ \    / /  ____| \ | |__   __/ ____|
-#  _| |_  | |__   \ \  / /| |__  |  \| |  | | | (___
-# |_   _| |  __|   \ \/ / |  __| | . ` |  | |  \___ \
-#   |_|   | |____   \  /  | |____| |\  |  | |  ____) |
-#         |______|   \/   |______|_| \_|  |_| |_____/
-# ============================================================================ #
+#  ________      ________ _   _ _______ _____
+# |  ____\ \    / /  ____| \ | |__   __/ ____|
+# | |__   \ \  / /| |__  |  \| |  | | | (___
+# |  __|   \ \/ / |  __| | . ` |  | |  \___ \
+# | |____   \  /  | |____| |\  |  | |  ____) |
+# |______|   \/   |______|_| \_|  |_| |_____/
 
 class JanetFileEvents (sublime_plugin.ViewEventListener):  
   def on_post_save_async(self):
@@ -123,14 +68,12 @@ class JanetFileEvents (sublime_plugin.ViewEventListener):
     print(filtered)
     return filtered
 
-# ============================================================================ #
-#          _      _____ ______ ______ _______     _______ _      ______
-#    _    | |    |_   _|  ____|  ____/ ____\ \   / / ____| |    |  ____|
-#  _| |_  | |      | | | |__  | |__ | |     \ \_/ / |    | |    | |__
-# |_   _| | |      | | |  __| |  __|| |      \   /| |    | |    |  __|
-#   |_|   | |____ _| |_| |    | |___| |____   | | | |____| |____| |____
-#         |______|_____|_|    |______\_____|  |_|  \_____|______|______|
-# ============================================================================ #
+#  _      _____ ______ ______ _______     _______ _      ______
+# | |    |_   _|  ____|  ____/ ____\ \   / / ____| |    |  ____|
+# | |      | | | |__  | |__ | |     \ \_/ / |    | |    | |__
+# | |      | | |  __| |  __|| |      \   /| |    | |    |  __|
+# | |____ _| |_| |    | |___| |____   | | | |____| |____| |____
+# |______|_____|_|    |______\_____|  |_|  \_____|______|______|
 
 ##
 ## plugin_loaded
@@ -148,145 +91,18 @@ def plugin_loaded():
     sublime.message_dialog('spork library for janet is not installed')
     return
 
-  # if is_nrepl_process_running():
-  #   set_nrepl_status_flag(True)
-  # else:
-  #   set_nrepl_status_flag(False)
-
 ##
 ## plugin_unloaded
 ##
 def plugin_unloaded():
-  # terminate_nrepl_proc()
   pass
 
-# ============================================================================ #
-#           _      ____   _____ _____ _____
-#          | |    / __ \ / ____|_   _/ ____|
-#  ______  | |   | |  | | |  __  | || |
-# |______| | |   | |  | | | |_ | | || |
-#          | |___| |__| | |__| |_| || |____
-#          |______\____/ \_____|_____\_____|
-# ============================================================================ #
-
-##
-## create_janet_temp
-##
-def get_temp_dir():
-  global PLUGIN_TEMP_DIR  
-  temp = pathlib.Path(sublime.cache_path(), PLUGIN_TEMP_DIR)  
-  if not temp.exists():
-    temp.mkdir(parents=True)
-  return temp
-
-##
-## get_proc_file
-##
-def get_proc_file():  
-  global PLUGIN_REPL_PROC
-  temp = get_temp_dir()
-  proc = pathlib.Path(sublime.cache_path(), temp, PLUGIN_REPL_PROC)  
-  if not proc.exists():
-    proc.touch()
-  return proc
-
-##
-## save_nrepl_pid
-##
-def save_nrepl_pid(pid):
-  procFile = get_proc_file()
-  with procFile.open('w') as proc:
-    proc.write(str(pid))
-
-##
-## get_nrepl_pid
-##
-def get_nrepl_pid():
-  procFile = get_proc_file()
-  with procFile.open('r') as proc:
-    return proc.read()
-  return None
-
-def does_pid_exist(pid):
-  pid = int(pid)
-  try:
-    if (pid == 0):
-      return False
-    os.getpgid(pid)    
-    return True
-  except Exception as err:    
-    print(err)
-    return False
-
-##
-## terminate_nrepl_proc
-##
-def terminate_nrepl_proc():
-  server_pid = get_nrepl_pid()
-  if (server_pid != 0):
-    stop_server(server_pid)
-    save_nrepl_pid(0)
-
-##
-## is_nrepl_process_healthy
-##
-def is_nrepl_process_healthy():    
-    host = configs_get(NREPL_HOST)
-    port = configs_get(NREPL_PORT)
-
-    client = start_client(host, port)    
-    if client == False:
-      return False
-
-    pong = nrepl_ping(client)    
-    if not pong:
-      return False
-
-    stop_client(client)
-    return True
-
-##
-## is_nrepl_process_running`
-##
-def is_nrepl_process_running():  
-  pid = get_nrepl_pid()  
-  if pid == None:
-    return False
-
-  if not does_pid_exist(pid):
-    return False
-
-  if not is_nrepl_process_healthy(): # or fucked up    
-    terminate_nrepl_proc()
-    return False
-
-  return True
-
-##
-## start_nrepl_process
-##
-def start_nrepl_process():
-  if is_nrepl_process_running():    
-    return True
-
-  janet = configs_get(JANET_EXEC)
-  host = configs_get(NREPL_HOST)
-  port = configs_get(NREPL_PORT)
-  server = start_server(janet, host, port)
-  if server == False:
-    return False
-  pid = server.get('pid')  
-  save_nrepl_pid(pid)
-  return True
-
-##
-## set_nrepl_status_flag
-##
-def set_nrepl_status_flag(is_running):
-  if is_running:
-    update_status_message('janet nrepl server is running', 'jide', '█')
-  else:
-    update_status_message('janet nrepl server is not running', 'jide', '░')
+#  _      ____   _____ _____ _____  _____
+# | |    / __ \ / ____|_   _/ ____|/ ____|
+# | |   | |  | | |  __  | || |    | (___
+# | |   | |  | | | |_ | | || |     \___ \
+# | |___| |__| | |__| |_| || |____ ____) |
+# |______\____/ \_____|_____\_____|_____/
 
 ##
 ## is_janet_installed
@@ -327,26 +143,6 @@ def configs_get(key):
 ##
 def configs_set(key, val):
   return configs_ref().set(key, val)
-
-##
-## update_status_message
-##
-def update_status_message(msg, key=None, val=None):
-  windows = sublime.windows()
-  for window in windows:
-    if (key != None and val != None):
-      view = window.active_view() 
-      view.set_status(key, val)
-    window.status_message(msg)
-
-##
-## is_janet_file
-##
-def is_janet_file(view):
-  selection = view.sel()[-1]
-  source = view.scope_name(selection.b)
-  return source.strip().startswith('source.janet')
-
 
 ##
 ## format_code_section
